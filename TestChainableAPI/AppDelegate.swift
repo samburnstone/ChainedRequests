3d81db55eb54722d8af44c68f6530122b187fc5c
Sam Burnstone <samburnstone@apples-MacBook-Pro.local>
2019-04-28 19:33:03 +0100

    Base project commit

---
+++ TestChainableAPI/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TestChainableAPI
+//
+//  Created by Sam Burnstone on 28/04/2019.
+//  Copyright © 2019 Sam Burnstone. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TestChainableAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestChainableAPI
+//
+//  Created by Sam Burnstone on 28/04/2019.
+//  Copyright © 2019 Sam Burnstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
